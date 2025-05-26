d78ffd2d2f6701e97902316f0de4c4681854d596
Asmaa-A7med <asmaaahmed02758@gmail.com>
2025-05-26 22:58:25 +0300

    save changes

--- ResturantDelivery/Dtos/CustomerDto.cs
+++ ResturantDelivery/Dtos/CustomerDto.cs
@@ -2,5 +2,10 @@
 {
     public class CustomerDto
     {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
     }
 }
